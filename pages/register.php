a30242b11eeb25ea35e9df057e3ba52ec0a4d470
delvilitata <tatadelviliyanti@gmail.com>
2024-06-10 13:27:21 +0700

    add register

---
+++ pages/register.php
@@ -0,0 +1,33 @@
+
+<div class="container">
+    <div class="row justify-content-center align-items-center my-4">
+        <div class="col-12 col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-title fw-bold fs-3 text-center">Register</div>
+                    <form>
+                        <div class="p-3">
+                            <div class="mb-3">
+                                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter your email">
+                            </div>
+                            <div class="mb-3">
+                                <input type="password" class="form-control" id="password" placeholder="Enter your password">
+                            </div>
+                            <div class="mb-3">
+                                <input type="text" class="form-control" id="name" placeholder="Enter your name">
+                            </div>
+                            <div class="mb-3">
+                                <input type="text" class="form-control" id="address" placeholder="Enter your address">
+                            </div>
+                            <div class="d-grid gap-2">
+                                <button class="btn btn-primary" type="button">Register</button>
+                                <div class="form-text text-center">Have an account?</div>
+                                <a class="btn btn-secondary" href="index.php?p=login">Log In</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
